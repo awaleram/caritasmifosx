366fd343a4e06361f68f34b42af314a6a2368e70
awaleram <ram.awale@confuxtechnologies.com>
2015-11-03 09:44:34 +0530

    caritas investment tracker for testing with all feature and migration schema on 03/11/2015

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V99019__investmentTracker_parameter_in_strechy_report_parameter_table.sql
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V99019__investmentTracker_parameter_in_strechy_report_parameter_table.sql
@@ -1,70 +1,70 @@
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
-(select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Maturity Start Date'),
+(select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Maturity Start Date')
 ,'maturityStartDate');
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Maturity End Date'),
-,'maturityEndDate');
+'maturityEndDate');
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Start Date'),
-,'investedStartDate');
+'investedStartDate');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment End Date'),
-,'investedEndDate');
+'investedEndDate');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Product'),
-,'loanProductId');
+'loanProductId');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Distribution Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Account No'),
-,'investmentAccountNo');
+'investmentAccountNo');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Maturity Start Date'),
-,'maturityStartDate');
+'maturityStartDate');
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Maturity End Date'),
-,'maturityEndDate');
+'maturityEndDate');
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Start Date'),
-,'investedStartDate');
+'investedStartDate');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment End Date'),
-,'investedEndDate');
+'investedEndDate');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Product'),
-,'loanProductId');
+'loanProductId');
 
 
 
 INSERT INTO `stretchy_report_parameter` (`report_id`, `parameter_id`, `report_parameter_name`) VALUES ( (select sr.id from stretchy_report sr where sr.report_name ='Investment Status Report'),
 (select sp.id   from stretchy_parameter sp where sp.parameter_name = 'Investment Account No'),
-,'investmentAccountNo');
+'investmentAccountNo');
 
 
 
