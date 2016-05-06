9210ba9875d4ccd452eefacae14abad73e532c88
awaleram <ram.awale@confuxtechnologies.com>
2016-05-06 12:47:19 +0530

    added new migration script for insert new transaction type for investment earning

--- mifosng-provider/src/main/resources/sql/migrations/core_db/V99038__Alter_ct_posted_investment_earning_for_adding_group_interest_earn_and_add_groupCharge-applied.sql
+++ mifosng-provider/src/main/resources/sql/migrations/core_db/V99038__Alter_ct_posted_investment_earning_for_adding_group_interest_earn_and_add_groupCharge-applied.sql
@@ -5,5 +5,3 @@ ALTER TABLE `ct_posted_investment_earnings`
 	
 	
 	
-	ALTER TABLE `ct_posted_investment_earnings`
-	ADD COLUMN `group_charge_applied` DECIMAL(10,2) NULL DEFAULT NULL AFTER `gorup_interest_earned`;
